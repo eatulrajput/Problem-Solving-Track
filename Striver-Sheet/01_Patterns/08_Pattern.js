0e6f4440dcdda81f4c20048e75bac16b6b2de972
Atul Rajput <eatulrajput@gmail.com>
2026-08-12 02:06:15 +0530

    Temple top pattern

---
+++ Striver-Sheet/01_Patterns/08_Pattern.js
@@ -0,0 +1,39 @@
+class Solution {
+  pattern1(n) {
+    for (let i = 0; i < n; i++) {
+      // Reset string for each row
+      let row = "";
+
+      //   Space
+      for (let j = 0; j <i + 1; j++) {
+        row += " ";
+      }
+
+      // Star
+      for (let j = 0; j < 2 * n - (2*i + 1); j++) {
+        row += "*";
+      }
+
+      // Space
+      for (let j = 0; j < i; j++) {
+        row += " ";
+      }
+      console.log(row);
+    }
+  }
+}
+
+// Instantiate the class and call the method
+const solution = new Solution();
+solution.pattern1(5);
+
+/*
+Solution
+
+ *********
+  ******* 
+   *****  
+    ***   
+     *   
+
+*/
